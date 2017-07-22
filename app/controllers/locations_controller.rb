99bf2ce314f41ace3933d4ad83d6048dba69075c
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-07-22 18:33:35 +0100

    add REST routes for Location model

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,36 +9,36 @@ class LocationsController < ApplicationController
     render :json => location
   end
 
-  # def create
-  #   leagueFinder = League.find( location_params[:league])
-  #   location= Location.create( {name: location_params[:name], league: leagueFinder } )
-  #   render json: location, status: :created
-  # end
+  def create
+    leagueFinder = League.find( location_params[:league])
+    location= Location.create( {name: location_params[:name], league: leagueFinder, lat: location_params[:lat], lng: location_params[:lng] } )
+    render json: location, status: :created
+  end
 
-  # def update
-  #   location= Location.find( params[:id])
-  #   leagueFinder = League.find( location_params[:league])
+  def update
+    location= Location.find( params[:id])
+    leagueFinder = League.find( location_params[:league])
 
-  #   if location.update_attributes( { name: location_params[:name], league: leagueFinder } )
-  #     render :json => location
-  #   else
-  #     render :json => { status: :update_failed }
-  #   end
-  # end
+    if location.update_attributes( { name: location_params[:name], league: leagueFinder, lat: location_params[:lat], lng: location_params[:lng] } )
+      render :json => location
+    else
+      render :json => { status: :update_failed }
+    end
+  end
 
-  # def destroy
-  #   location= Location.find( params[:id] )
-  #   if location.destroy
-  #     render :json =>  { status: :success }
-  #   else
-  #     render :json =>  { status: :delete_failed }
-  #   end
-  # end  
+  def destroy
+    location= Location.find( params[:id] )
+    if location.destroy
+      render :json =>  { status: :success }
+    else
+      render :json =>  { status: :delete_failed }
+    end
+  end  
 
   private
 
   def location_params
-    params.require(:location).permit([:name, :league])
+    params.require(:location).permit([:name, :league, :lat, :lng])
   end
 
 end
