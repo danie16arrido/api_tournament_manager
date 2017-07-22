6155de91a1fbafeb1bf61820640beb97e7c2a32d
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-07-22 13:29:34 +0100

    pass tests for Team Model

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Teams have name" do
+    assert_equal( "Team A", teams(:one).name)
+  end
+
+  test "Teams have league id" do
+    assert_equal( 5, teams(:two).id)
+  end
 end
