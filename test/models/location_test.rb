138ac605bb70d18e1a6a9b1bc0a2dc91c3e2b6ff
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-07-22 18:47:07 +0100

    pass test for Location model

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "location has name" do  
+    assert_equal("fieldB", locations(:one).name)
+  end
+
+  test "location has leagueid" do  
+    assert_equal(2, locations(:one).league_id)
+  end
+
+  test "location has lat" do  
+    assert_equal(14.5, locations(:two).lat)
+  end
+
+  test "location has lng" do  
+    assert_equal(0.512349, locations(:two).lng)
+  end
+
 end
