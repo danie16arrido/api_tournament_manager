4845f98261e151f11d584b0f7e9369d18efde8e1
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-07-22 18:00:10 +0100

    add Player REST routes and db queries for each route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   scope path: "api/v1" do
     resources :leagues, defaults: {format: :json}
     resources :teams, defaults: {format: :json}
+    resources :players, defaults: {format: :json}
   end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
