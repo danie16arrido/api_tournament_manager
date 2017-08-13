220d499f447d122cc731a2a9bde027dbb7b5978c
danie1garrido <daniel.garrido.info@gmail.com>
2017-08-13 14:05:43 +0100

    rebuild schema 1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722210851) do
+ActiveRecord::Schema.define(version: 20170722205601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(version: 20170722210851) do
     t.integer  "player_id"
     t.integer  "match_id"
     t.integer  "minute"
+    t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "category"
   end
 
   add_index "goals", ["match_id"], name: "index_goals_on_match_id", using: :btree
